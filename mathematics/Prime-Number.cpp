cc7a3a5c01a7181bd0109b0a6db74db442d03f41
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-06-12 22:47:23 +0530

    GeeksforGeeks

--- mathematics/Prime-Number.cpp
+++ mathematics/Prime-Number.cpp
@@ -7,14 +7,13 @@ class Solution{
 public:
     int isPrime(int N){
         // code here
-        if(N == 1) {
+        if (N == 1)
+        return 0;
+        if(N == 2 || N == 3) return 1;
+        if(N % 2 == 0 || N % 3 == 0) return 0;
+        for(int i = 5; i*i <= N; i=i+6) {
+            if(N % i == 0 || N % (i + 2) == 0)
             return 0;
-        }
-        
-        for(int i = 2; i*i <= N; i++) {
-            if(N % i == 0) {
-                return 0;
-            }
         }
         return 1;
     }
