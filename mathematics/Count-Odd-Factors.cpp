cedfc1ec497ce0abb7a37dce1490ddce7886153c
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-06-24 02:31:30 +0530

    GeeksforGeeks

---
+++ mathematics/Count-Odd-Factors.cpp
@@ -0,0 +1,27 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+    int count(int N){
+        // code here
+        return sqrt(N);
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.count(N) << endl;
+    }
+    return 0; 
+}  // } Driver Code Ends
