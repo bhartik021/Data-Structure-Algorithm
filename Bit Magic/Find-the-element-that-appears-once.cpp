102d68c5e6e9ff6918ebc649de71295ed818720c
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-06-24 15:14:25 +0530

    GeeksforGeeks

---
+++ Bit Magic/Find-the-element-that-appears-once.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int search(int A[], int N){
+	    //code
+	    int count = 0;
+	    for(int i = 0; i < N; i++) {
+	        count = count^A[i];
+	    }
+	    return count;
+	}
+};
+
+// { Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t,len;
+    cin>>t;
+    while(t--)
+    {
+        cin>>len;
+        int arr[len];
+        for(int i=0;i<len;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.search(arr, len)<<'\n';
+    }
+    return 0;
+}
+  // } Driver Code Ends
