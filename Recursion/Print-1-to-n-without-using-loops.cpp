0cf47e2bbb83940e38bca71ca9e3aa729044f4d0
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-07-01 16:26:37 +0530

    GeeksforGeeks

---
+++ Recursion/Print-1-to-n-without-using-loops.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    void printTillN(int N)
+    {
+        // Write Your Code here
+        if(N == 0) return;
+        printTillN(N - 1);
+        cout << N << " ";
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        ob.printTillN(N);
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
