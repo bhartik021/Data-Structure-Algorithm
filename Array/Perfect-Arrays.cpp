c58e099790627174214a177fff50f46f1c8bf30c
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-10-05 11:50:44 +0530

    GeeksforGeeks

---
+++ Array/Perfect-Arrays.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool IsPerfect(int a[],int n)
+    {
+        // Complete the function
+        for(int i = 0; i <n ; i++) {
+           if(a[i] != a[n-i-1]) return false;
+        }
+        return true;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        if(ob.IsPerfect(a,n))
+            cout<<"PERFECT\n";
+        else
+            cout<<"NOT PERFECT\n";
+        
+    }
+    
+}
+// } Driver Code Ends
