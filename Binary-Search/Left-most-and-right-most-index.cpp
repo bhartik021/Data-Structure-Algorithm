600ff7da520ba703d3223c851320b40402fdae2c
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-10-08 17:55:01 +0530

    GeeksforGeeks

---
+++ Binary-Search/Left-most-and-right-most-index.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    pair<long,long> indexes(vector<long long> v, long long x)
+    {
+        // code here
+        int n = v.size();
+        int first = lower_bound(v.begin(), v.end(), x) - v.begin();
+        int last = upper_bound(v.begin(), v.end(), x) - v.begin();
+        
+        if(first < n && v[first] == x) {
+            return{first, last - 1};
+        }
+        return {-1, -1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n, k;
+        vector<long long>v;
+        cin>>n;
+        for(long long i=0;i<n;i++)
+        {
+            cin>>k;
+            v.push_back(k);
+        }
+        long long x;
+        cin>>x;
+        Solution obj;
+        pair<long,long> pair = obj.indexes(v, x);
+        
+        if(pair.first==pair.second and pair.first==-1)
+        cout<< -1 <<endl;
+        else
+        cout<<pair.first<<" "<<pair.second<<endl;
+
+    }
+	return 0;
+}
+
+// } Driver Code Ends
