017707cb5ca490a6104af4fe940414e82e47ab51
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-10-20 21:42:22 +0530

    GeeksforGeeks

--- Array/Find-minimum-and-maximum-element-in-an-array.cpp
+++ Array/Find-minimum-and-maximum-element-in-an-array.cpp
@@ -1,4 +1,53 @@
-// { Driver Code Starts
+// // { Driver Code Starts
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+
+// pair<long long, long long> getMinMax(long long a[], int n) ;
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int n;
+//         cin >> n;
+//         ll a[n];
+//         for (int i = 0; i < n; i++) cin >> a[i];
+
+//         pair<ll, ll> pp = getMinMax(a, n);
+
+//         cout << pp.first << " " << pp.second << endl;
+//     }
+//     return 0;
+// }// } Driver Code Ends
+
+
+// pair<long long, long long> getMinMax(long long a[], int n) {
+//     long long int mine = 1000000000001;
+//     long long int maxe = 0;
+    
+//     // max 
+//     for(int i = 0; i < n; i++) {
+//         if(a[i] > maxe) {
+//             maxe = a[i];
+//         }
+//     }
+    
+//     // main 
+//     for(int i = 0; i < n; i++) {
+//         if(a[i] < mine) {
+//             mine = a[i];
+//         }
+//     }
+    
+//     pair<long long, long>ans;
+//     ans.first = mine;
+//     ans.second = maxe;
+    
+//     return ans;
+// }
+
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -19,30 +68,23 @@ int main() {
         cout << pp.first << " " << pp.second << endl;
     }
     return 0;
-}// } Driver Code Ends
+}
+// } Driver Code Ends
 
 
 pair<long long, long long> getMinMax(long long a[], int n) {
-    long long int mine = 1000000000001;
-    long long int maxe = 0;
-    
-    // max 
-    for(int i = 0; i < n; i++) {
-        if(a[i] > maxe) {
-            maxe = a[i];
-        }
-    }
-    
-    // main 
-    for(int i = 0; i < n; i++) {
-        if(a[i] < mine) {
-            mine = a[i];
-        }
-    }
-    
-    pair<long long, long>ans;
-    ans.first = mine;
-    ans.second = maxe;
-    
-    return ans;
+ pair<long long, long long>p;
+ 
+ p.first = a[0];
+ p.second = a[0];
+ 
+ for(int i = 1; i < n; i++) {
+     if(p.first > a[i]){
+         p.first = a[i];
+     }
+     if(p.second < a[i]){
+         p.second = a[i];
+     }
+ }
+ return p;
 }
