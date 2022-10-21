c9ee61e9aa90f2f4fe829bf62df1723f660cdcd1
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-10-21 17:37:09 +0530

    GeeksforGeeks

---
+++ Array/Largest-Element-in-Array.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        // sort(arr.begin(), arr.end());
+        // return arr[n-1];
+        
+        int res = arr[0];
+        
+        for(int i = 0; i < n; i++) {
+            if(arr[i] > res){
+                res = arr[i];
+            }
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.largest(arr, n) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
