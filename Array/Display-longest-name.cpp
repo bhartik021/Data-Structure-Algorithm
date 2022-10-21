233656cf787f21e6d021950a1e57a16896baeeb1
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-10-21 18:09:52 +0530

    GeeksforGeeks

---
+++ Array/Display-longest-name.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+       string res = names[0];
+       
+       for(int i = 0; i < n; i++) {
+           if(names[i].size() > res.size())
+           res = names[i];
+       }
+       return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+
+// } Driver Code Ends
