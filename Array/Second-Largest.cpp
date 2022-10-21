34dbb105669706c2dce2b64479237d98c814ee8c
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-10-21 17:53:34 +0530

    GeeksforGeeks

---
+++ Array/Second-Largest.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here
+	    sort(arr, arr+n);
+	    int i = 0;
+	    while(i < n) {
+	        if(arr[n-1] != arr[n-1-i]){
+	            return arr[n-1-i];
+	        }else {
+	            i++;
+	        }
+	    }
+	    return -1;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.print2largest(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
