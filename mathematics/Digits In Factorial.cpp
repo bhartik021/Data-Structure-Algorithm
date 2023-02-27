996cf3b15875c585203b8478483f1094d54b6405
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-02-27 16:57:43 +0530

    GFG

---
+++ mathematics/Digits In Factorial.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int digitsInFactorial(int N)
+    {
+        // code here
+        if(N < 0) return 0;
+        if(N <= 1) return 1;
+        double res = 0;
+        for(int i = 2; i <= N; i++) {
+            res += log10(i);
+        }
+        return floor(res) + 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+    
+    //taking testcases
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        
+        //taking N
+        cin>>N;
+        Solution ob;
+        //calling method digitsInFactorial()
+        cout<<ob.digitsInFactorial(N)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
