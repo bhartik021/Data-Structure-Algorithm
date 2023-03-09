2677a28e7fdd712f64720dbcf4d5fa8b3519ed77
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-03-09 17:24:12 +0530

    GeeksforGeeks

---
+++ String/Remove Spaces.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        //code here.
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == ' ') {
+                s[i] = '\0';
+            }
+        }
+        return s;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
