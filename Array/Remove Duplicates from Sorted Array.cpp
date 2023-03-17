6c0b030f2b4dda3062eac6b9038a962c768b10da
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-03-17 14:43:16 +0530

    GeeksforGeeks

---
+++ Array/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+	int i = 0;
+	for(int j = 1; j < n; j++) {
+        if (arr[i] != arr[j]) {
+            arr[i + 1] = arr[j];
+            i++;
+        }
+    }
+    return (i+1);
+}
