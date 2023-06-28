9c700c7a8dc1a4ce712acb069881f42c3d61151e
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-06-28 19:00:18 +0530

    GeeksforGeeks

---
+++ Array/Rotation.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int findKRotation(int arr[], int n) {
+	    // code here
+	    int minIndex = 0;
+	    for(int i = 1; i < n; i++) {
+	        if(arr[i] < arr[minIndex])
+	        minIndex = i;
+	    }
+	    return minIndex;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findKRotation(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
