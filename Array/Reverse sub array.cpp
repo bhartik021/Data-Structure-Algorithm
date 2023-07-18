a559350a90b3934b7a77448824a1a25d1e95862a
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-07-18 21:43:33 +0530

    GeeksforGeeks

---
+++ Array/Reverse sub array.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	void reverseSubArray(int *arr, int n, int l, int r) {
+	    int left = l - 1;
+	    int right = r - 1;
+	    // code here
+	    while(left < right) {
+	        swap(arr[left], arr[right]) ;
+	        left++;
+	        right--;
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, l, r;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> l >> r;
+        Solution ob;
+        ob.reverseSubArray(arr, n, l, r);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
