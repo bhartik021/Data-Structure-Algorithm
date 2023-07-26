344c514f3a954474aa17dc71d38a101d7040e24c
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-07-26 17:04:44 +0530

    Coding Ninjas

---
+++ Linked List/Length of Linked List.cpp
@@ -0,0 +1,40 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+int length(Node *head)
+{
+	//Write your code here
+    int count = 0;
+    Node* current = head;
+    while(current != NULL) {
+        count++;
+        current = current -> next;
+    }
+    return count;
+}
