868380c70bf37623026f561156a0b4d67af6b2e3
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-08-08 13:00:28 +0530

    GeeksforGeeks

---
+++ Linked List/Next Greater Element.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        stack<long long>s;
+        s.push(-1);
+        vector<long long>ans(n, 0);
+        for(int i = n - 1; i >= 0; i--) {
+            long long current = arr[i];
+            while(s.top() != -1 && s.top() <= current) {
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push(current);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
