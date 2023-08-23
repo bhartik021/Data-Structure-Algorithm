3ef4d0cfad8b6c5d0e09927829413e5a94528832
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-08-23 13:38:23 +0530

    LeetCode

---
+++ Tree/222. Count Complete Tree Nodes.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
+        int l = countNodes(root->left);
+        int r = countNodes(root->right);
+        
+        int sum = l + r + 1;
+        
+        return sum;
+    }
+};
