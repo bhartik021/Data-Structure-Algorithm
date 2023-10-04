b17e4115985f638b5dbca86ae39171b567cba052
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-10-04 20:31:43 +0530

    Create Code : Remove Min.cpp

---
+++ Priority Queue/Code : Remove Min.cpp
@@ -0,0 +1,77 @@
+#include <vector>
+
+class PriorityQueue {
+    vector<int> pq;
+
+   public:
+    bool isEmpty() { 
+        return pq.size() == 0; 
+    }
+
+    int getSize() { 
+        return pq.size(); 
+    }
+
+    int getMin() {
+        if (isEmpty()) {
+            return 0;
+        }
+
+        return pq[0];
+    }
+
+    void insert(int element) {
+        pq.push_back(element);
+
+        int childIndex = pq.size() - 1;
+
+        while (childIndex > 0) {
+            int parentIndex = (childIndex - 1) / 2;
+
+            if (pq[childIndex] < pq[parentIndex]) {
+                int temp = pq[childIndex];
+                pq[childIndex] = pq[parentIndex];
+                pq[parentIndex] = temp;
+            } else {
+                break;
+            }
+
+            childIndex = parentIndex;
+        }
+    }
+
+    int removeMin() {
+        if(isEmpty()) {
+            return 0; // Priority Queue is empty
+        }
+        int ans = pq[0];
+        pq[0] = pq[pq.size() - 1];
+        pq.pop_back();
+
+        // Down-Heapify
+        int parentIndex = 0;
+        int leftChildIndex = 2 * parentIndex + 1;
+        int rightChildIndex = 2 * parentIndex + 2;
+
+        while(leftChildIndex < pq.size()) {
+            int minIndex = parentIndex;
+            if(pq[minIndex] > pq[leftChildIndex]) {
+                minIndex = leftChildIndex;
+            }
+            if(rightChildIndex < pq.size() && pq[minIndex] > pq[rightChildIndex]) {
+                minIndex = rightChildIndex;
+            }
+            if(minIndex == parentIndex) {
+                break;
+            }
+            int temp = pq[minIndex];
+            pq[minIndex] = pq[parentIndex];
+            pq[parentIndex] = temp;
+
+            parentIndex = minIndex;
+            leftChildIndex = 2 * parentIndex + 1;
+            rightChildIndex = 2 * parentIndex + 2;
+        }
+        return ans;
+    }
+};
