cd267654d5670709b1807df09777f078bc6c4adc
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-10-31 15:48:20 +0530

    Create Chocolate Distribution Problem.cpp

---
+++ Greedy/Chocolate Distribution Problem.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    sort(a.begin(), a.end());
+    
+    int i = 0;
+    int j = m - 1;
+    
+    int mini = INT_MAX;
+    
+    while(j < a.size()) {
+        int difference = a[j] - a[i];
+        mini = min(mini, difference);
+        i++;
+        j++;
+    }
+    return mini;
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
