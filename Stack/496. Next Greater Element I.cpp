6941544950873cbfa8c9f41b36c82093948f0bcc
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-11-03 23:56:10 +0530

    Create 496. Next Greater Element I.cpp

---
+++ Stack/496. Next Greater Element I.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    unordered_map<int, int>umap;
+    int n = nums2.size();
+    stack<int>st;
+    for(int i = n - 1; i >= 0; i--) {
+        int ele = nums2[i];
+        while(!st.empty() && st.top() < ele) {
+            st.pop();
+        }
+        int res = (st.empty() ? -1 : st.top());
+        umap.insert({ele, res});
+        st.push(ele);
+    }    
+    vector<int>ans;
+    for(auto x : nums1) {
+        ans.push_back(umap[x]);
+    }
+    return ans;
+    }
+};
+
