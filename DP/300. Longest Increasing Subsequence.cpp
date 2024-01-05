cec36786e80635c62be7c00e26cc6c987db408d3
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2024-01-05 22:47:17 +0530

    Create 300. Longest Increasing Subsequence.cpp

---
+++ DP/300. Longest Increasing Subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLIS(std::vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+
+        int n = nums.size();
+        std::vector<int> dp(n, 1);
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = std::max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return *std::max_element(dp.begin(), dp.end());
+    }
+};
