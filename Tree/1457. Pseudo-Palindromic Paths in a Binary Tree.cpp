b1e0bda8279c9a05482bbf93f3ea4929b89b090a
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2024-01-24 22:48:43 +0530

    Create 1457. Pseudo-Palindromic Paths in a Binary Tree.cpp

---
+++ Tree/1457. Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pseudoPalindromicPaths (TreeNode* root) {
+    int count = 0, path = 0;
+
+        stack<pair<TreeNode*, int>> stk;
+        stk.push({root, 0});
+
+        while (!stk.empty()) {
+            auto [node, path] = stk.top();
+            stk.pop();
+
+            if (node != nullptr) {
+                
+                path = path ^ (1 << node->val);
+
+                
+                if (node->left == nullptr && node->right == nullptr) {
+                    
+                    if ((path & (path - 1)) == 0) {
+                        ++count;
+                    }
+                } else {
+                    stk.push({node->right, path});
+                    stk.push({node->left, path});
+                }
+            }
+        }
+
+        return count;    
+    }
+};
