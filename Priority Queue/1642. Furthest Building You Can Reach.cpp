3521a1b9c887a83ca1ffbc80c76f7cb1a52c3dfb
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2024-02-17 21:49:35 +0530

    Create 1642. Furthest Building You Can Reach.cpp

---
+++ Priority Queue/1642. Furthest Building You Can Reach.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& h, int b, int l) {
+        
+        // Priority Queue for storing the bricks used in each step in decreasing order (Max at top)
+        priority_queue<int> p;
+    
+        int i=0, diff =0; 
+        for(i=0; i<h.size()-1; i++){ 
+            
+            diff = h[i+1]-h[i];
+            
+            if(diff <= 0){
+                continue;
+            }
+
+            b -= diff; 
+            p.push(diff); 
+       
+            if(b < 0){
+                b += p.top(); 
+                p.pop(); 
+                l--;
+            }
+
+          
+            if(l < 0) break;
+        }
+        
+        
+        return i;
+    }
+};
+
