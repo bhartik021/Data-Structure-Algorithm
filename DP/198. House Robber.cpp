bd7bd8f448e48465d8788428e490d9086cecfd54
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2024-03-21 21:05:30 +0530

    Update 198. House Robber.cpp

--- DP/198. House Robber.cpp
+++ DP/198. House Robber.cpp
@@ -1,21 +1,51 @@
+// Brute Force
+// class Solution {
+//     public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         int prev = 0, prev2 = 0, current;
+//         for(auto x : nums) {
+//             current = max(prev, prev2 + x);
+//             prev2 = prev;
+//             prev = current;
+//         }
+//         return current;
+//     }
+// };
+
+// Using Recursion (TLE)
+// class Solution {
+// public:
+// int robHelper(vector<int>&nums, int start) {
+//     if(start >= nums.size()) return 0;
+
+//     int robCurrent = nums[start] + robHelper(nums, start + 2);
+//     int robSkip = robHelper(nums, start + 1);
+
+//     return max(robCurrent, robSkip);
+// }
+//     int rob(vector<int>& nums) {
+//     return robHelper(nums, 0);  
+//     }
+// };
+
+// Tabulation (DP)
 class Solution {
-public:
+    public:
     int rob(vector<int>& nums) {
-      int n = nums.size();
-        
+        int n = nums.size();
+
         if(n == 0) return 0;
         if(n == 1) return nums[0];
-        
-        vector<int> dp(n);
-        
+
+        vector<int>dp(n);
+
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
-        
-        for(int i = 2; i < n; i++){
-            //if we rob the ith house, we can add dp[i-2] because there is no alear
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
         }
-        
-        return dp[n-1];   
+        return dp[n - 1];
     }
 };
