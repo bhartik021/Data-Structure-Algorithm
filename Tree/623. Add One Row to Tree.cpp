61611c96ae4707bcf851eb54cf234d26b0eb5925
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2024-04-16 21:25:27 +0530

    Create 623. Add One Row to Tree.cpp

---
+++ Tree/623. Add One Row to Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+     if (depth==1){
+            return new TreeNode(val, root, NULL);
+        }
+        vector<pair<TreeNode*,int>> stack={{root, 1}};
+        while(!stack.empty()){
+            auto [node, level]=stack.back();
+            stack.pop_back();
+            if (level==depth-1){
+                TreeNode* ptr=node->left;
+                node->left=new TreeNode(val, ptr, NULL);
+
+                ptr=node->right;
+                node->right=new TreeNode(val, NULL, ptr);
+            }
+            else{
+                if (node->right) stack.emplace_back(node->right, level+1);
+                if (node->left) stack.emplace_back(node->left, level+1);
+            }
+        }
+        return root;   
+    }
+};
